34f93558c615efee3dd74536b52e601fcc72edbe
bielprze <33664570+bielprze@users.noreply.github.com>
2017-11-14 17:41:00 +0100

    Add files via upload

---
+++ VideoArea.hpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   VideoArea.hpp
+ * Author: imam
+ *
+ * Created on 3 October 2015, 1:03 AM
+ */
+
+#ifndef VIDEOAREA_HPP
+#define	VIDEOAREA_HPP
+
+#include <gtkmm/drawingarea.h>
+
+#include <cairomm/context.h>
+#include <opencv2/opencv.hpp>
+
+class VideoArea : public Gtk::DrawingArea
+{
+    protected:
+        cv::VideoCapture cv_cap;
+        bool cv_opened;
+        virtual bool on_draw (const Cairo::RefPtr<Cairo::Context> &cr);
+        bool on_timeout ();
+    public:
+        VideoArea ();
+        virtual ~VideoArea();   
+};
+
+#endif	/* VIDEOAREA_HPP */
+
